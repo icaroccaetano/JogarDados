00c3c787dcda099bc89e82cb91a367c58ac7149b
icaroccaetano <icaroccaetano@gmail.com>
2024-03-31 16:49:38 -0300

    Adicionar arquivos de projeto.

---
+++ JogarDados.Dominio/JogarDadosSim.cs
@@ -0,0 +1,70 @@
+﻿namespace JogarDados.Dominio
+{
+    public class JogarDadosSim
+    {
+        public int SimularDados (String jog1, String jog2, int nrodadas)
+        {
+            int pont1 = 0, pont2 = 0;
+            int dado1 = 0, dado2 = 0;
+            int rodada = 1;
+            
+            Random r = new Random ();
+
+            //Rodadas iniciais 
+
+            for (; nrodadas > 0; nrodadas--)
+            {
+                dado1 = r.Next(1, 6);
+                dado2 = r.Next(1, 6);
+
+                if (dado1 > dado2)
+                {
+                    pont1++;
+                    Console.WriteLine(jog1 + " tirou o numero " + dado1 + " e " + jog2 + " o numero " + dado2 + ". " + jog1 + " venceu a rodada" + rodada + ".");
+                }
+                else if (dado1 < dado2)
+                {
+                    pont2++;
+                    Console.WriteLine(jog1 + " tirou o numero " + dado1 + " e " + jog2 + " o numero " + dado2 + ". " + jog2 + " venceu a rodada" + rodada + ".");
+                }
+                else
+                {
+                    Console.WriteLine(jog1 + " tirou o numero " + dado1 + " e " + jog2 + " o numero " + dado2 + ". Empate");
+                }
+
+                Console.WriteLine("Pontos do jogador " + jog1 + ": " + pont1 + ". Pontos do jogador " + jog2 + ": " + pont2);
+                rodada++;
+                Console.WriteLine();
+            }
+            // Desempate -> CASO NECESSARIO
+            while (pont1 == pont2)
+            {
+                for (; nrodadas > 0; nrodadas--)
+                    dado1 = r.Next(1, 6);
+                    dado2 = r.Next(1, 6);
+
+                    if (dado1 > dado2)
+                    {
+                        pont1++; 
+                        Console.WriteLine(jog1 + " tirou o numero " + dado1 + " e " + jog2 + " o numero " + dado2 + ". " + jog1 + " venceu a rodada" + rodada + ".");
+                    }
+                    else if (dado1 < dado2)
+                    {
+                        pont2++;
+                        Console.WriteLine(jog1 + " tirou o numero " + dado1 + " e " + jog2 + " o numero " + dado2 + ". " + jog2 + " venceu a rodada" + rodada + ".");
+                    }
+                    else
+                    {
+                        Console.WriteLine(jog1 + " tirou o numero " + dado1 + " e " + jog2 + " o numero " + dado2 + ". Empate");
+                    }
+                
+                Console.WriteLine("Pontos do jogador " + jog1 + ": " + pont1 + ". Pontos do jogador " + jog2 + ": " + pont2);
+                rodada++;
+                Console.WriteLine();
+            }
+
+            if (pont1 > pont2) return 1;
+            else return 2;  
+        }
+    }
+}
---
+++ JogarDados/Program.cs
@@ -0,0 +1,69 @@
+﻿using JogarDados.Dominio;
+using System;
+
+namespace Program
+{
+    public class JogarDadosMain
+    {
+        public static void Main(string[] args)
+        {
+            bool ctrl = true;
+            string jogador1 = null, jogador2 = null;
+            int vencedor, rodadas = 0;
+
+            // Instanciando objeto JogarDadosSim localmente
+            JogarDadosSim jogo = new JogarDadosSim();
+
+
+
+            // Loop para garantir nomes de jogadores diferentes
+            while (ctrl)
+            {
+                Console.WriteLine("Nome jogador 1:");
+                jogador1 = Console.ReadLine();
+
+                Console.WriteLine("Nome jogador 2:");
+                jogador2 = Console.ReadLine();
+
+                if (jogador1 == jogador2)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ERRO: Ambos os nomes iguais.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    ctrl = false;
+                }
+            }
+
+            ctrl = true;
+
+            // Defina o número de rodadas que você deseja
+            while (ctrl)
+            {
+                Console.WriteLine("Numero de rodadas: (Entre 1 e 5)");
+                rodadas = int.Parse(Console.ReadLine());
+
+                if (rodadas < 1 || rodadas > 5)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ERRO: Valor deve ser entre 1 e 5.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    ctrl = false;
+                }
+
+            }
+
+
+            // Chame o método SimularDados usando o objeto jogo
+            vencedor = jogo.SimularDados(jogador1, jogador2 ,rodadas);
+
+            // Exiba o vencedor do jogo
+            Console.WriteLine("O jogador " + (vencedor == 1 ? jogador1 : jogador2) + " venceu o jogo!");
+        }
+    }
+}
